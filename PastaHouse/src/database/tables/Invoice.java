c9d70506689acc4a0adc08898df15732fe4e09ac
RobinDM <archercross@gmail.com>
2012-12-12 15:23:22 +0100

    f1: Prepared invoice class

--- PastaHouse/src/database/tables/Invoice.java
+++ PastaHouse/src/database/tables/Invoice.java
@@ -4,9 +4,33 @@
  */
 package database.tables;
 
+import database.extra.InvoiceItem;
+import database.extra.Record;
+import java.util.List;
+
 /**
  *
  * @author Robin jr
  */
-public class Invoice {
+public class Invoice extends Record{
+    private int number;
+    private String date;
+    private Client client;
+    private String priceCode;
+    private double save;
+    private List<InvoiceItem> items;
+
+    public Invoice(int id, String... rest){
+	super(id, "atable");
+    }
+    
+    @Override
+    public boolean update() {
+	throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean delete() {
+	throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
