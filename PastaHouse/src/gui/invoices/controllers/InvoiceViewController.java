762d1bd5ecd7a2d37321c732047cef0a6f5b42fd
RobinDM <archercross@gmail.com>
2013-02-16 15:12:00 +0100

    restored printing to 1 invoice per print

--- PastaHouse/src/gui/invoices/controllers/InvoiceViewController.java
+++ PastaHouse/src/gui/invoices/controllers/InvoiceViewController.java
@@ -387,21 +387,21 @@ public class InvoiceViewController extends javax.swing.JPanel implements MasterD
 	    rijen[added++]=table.convertRowIndexToModel(i);
 	}
 	
-        Book b = new Book();
-        PageFormat pf = new PageFormat();
-        pf.setPaper(new A4());
+//        Book b = new Book();
+//        PageFormat pf = new PageFormat();
+//        pf.setPaper(new A4());
 
 //	MultiPrintable mp = new MultiPrintable();
 	
-        for (int i : rijen) {
-            b.append(new PrintableInvoiceNew(tableModel.getInvoiceAtRow(i)), pf);
+//        for (int i : rijen) {
+//            b.append(new PrintableInvoiceNew(tableModel.getInvoiceAtRow(i)), pf);
 //	    mp.add(new PrintableInvoiceNew(tableModel.getInvoiceAtRow(i)));
-        }
+//        }
 	
-        printer.Printer.driver().setPrintableBook(b);
+//        printer.Printer.driver().setPrintableBook(b);
 	
-//	Printable p = new PrintableInvoiceNew(tableModel.getInvoiceAtRow(rijen[0]));
-//        printer.Printer.driver().setPrintableJob(p);
+	Printable p = new PrintableInvoiceNew(tableModel.getInvoiceAtRow(rijen[0]));
+        printer.Printer.driver().setPrintableJob(p);
 //        printer.Printer.driver().setPrintableJob(mp, pf);
         printer.Printer.driver().tryPrint();
     }//GEN-LAST:event_printActionPerformed
